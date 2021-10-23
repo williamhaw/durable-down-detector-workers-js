37392b2a73848f7bd389512488d51e5f4fa55c19
William Haw <me@williamhaw.com>
2021-10-24 01:17:03 +0800

    Add first cut of UptimeActor

---
+++ src/uptime-actor.mjs
@@ -0,0 +1,35 @@
+import { DateTime, Interval } from 'luxon'
+
+export class UptimeActor {
+  constructor(state, env) {}
+
+  async fetch(request) {
+    try {
+      const start = DateTime.now()
+      const response = await fetch(state.id, { method: 'GET' })
+      const roundTripInterval = Interval(start, DateTime.now())
+
+      let status = ''
+      if (response.status <= 299) {
+        status = 'OK'
+      } else {
+        status = 'ERROR'
+      }
+
+      const data = {
+        status: status,
+        time: start.toUTC().toISO(),
+        roundTripMs: roundTripInterval.length,
+      }
+
+      const responseBody = {
+        monitorStatus: 'OK',
+        data: data,
+      }
+
+      return new Response(JSON.stringify(responseBody))
+    } catch (error) {
+      return new Response(JSON.stringify({ monitorStatus: 'ERROR' }))
+    }
+  }
+}
