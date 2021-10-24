95b7bc07556dc14725ca16400a716a3e88a559b4
William Haw <me@williamhaw.com>
2021-10-24 21:50:51 +0800

    Fix routing and request firing in index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -4,33 +4,39 @@ export { UptimeActor } from './uptime-actor.mjs'
 
 export default {
   async fetch(request, env) {
-    try {
-      return await handleRequest(request, env)
-    } catch (e) {
-      return new Response(e.message, { status: 500 })
-    }
+    return await handleRequest(request, env)
   },
 }
 
 async function handleRequest(request, env) {
   const path = new URL(request.url).pathname
 
+  console.log(`Path requested: ${path}`)
+
   switch (path) {
-    case 'schedule':
-      const urls = await env.URLS.list().keys.map(k => k.name)
+    case '/schedule':
+      const k = await env.URLS.list()
+      const urls = k.keys.map(k => k.name)
+
+      console.log(`URLS: ${urls}`)
 
       let responseData = {}
 
-      urls.foreach(async function(url) {
-        const id = env.UPTIME.idFromName(url)
-        const actor = env.UPTIME.get(id)
-        const response = await actor.fetch()
-        responseData[url] = response
-      })
+      await Promise.all(
+        urls.map(async url => {
+          const id = env.UPTIME.idFromName(url)
+          console.log(`object id: ${id}`)
+          const actor = env.UPTIME.get(id)
+          const response = await actor.fetch(new Request(url))
+          responseData[url] = response.json()
+          console.log(`response: ${JSON.stringify(response)}`)
+          console.log(`responseData: ${JSON.stringify(responseData)}`)
+        }),
+      )
 
       return new Response(JSON.stringify({ data: responseData }))
 
-    case 'dashboard':
+    case '/dashboard':
       return new Response()
 
     default:
